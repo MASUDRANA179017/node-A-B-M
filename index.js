0d6f53e9ec9f8475fa668063d3883761959ac42c
Masud Rana <68780186+MASUDRANA179017@users.noreply.github.com>
2025-09-01 00:34:21 +0600

    call Emitter on and once

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+// events = ঘটনা
+// which refers to anything that happens
+
 const EventEmitter = require("events")
 class Emitter extends EventEmitter {}
 
 const MyEmitter = new Emitter();
 
 MyEmitter.on("abc", () => {
-  console.log("My Event is running");
+  console.log("My Event is running 1");
+});
+MyEmitter.on("abc", () => {
+  console.log("My Event is running 2");
+});
+MyEmitter.on("abc", () => {
+  console.log("My Event is running 3");
+});
+// its run only one time 
+MyEmitter.once("abc", () => {
+  console.log("My Event is running once");
 });
 
+// 2 time calls abc "emitter.on" call 
 MyEmitter.emit("abc");
+MyEmitter.emit("abc");
+
